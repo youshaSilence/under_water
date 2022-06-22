92e3e97303d13516baa9e4e264891630bccc3094
youshaSilence <mksyousha2014@gmail.com>
2022-06-22 14:32:42 +0300

    Cleaning

--- configuration/environment.js
+++ configuration/environment.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   paths: {
     /* Path to source files directory */
-    source: path.resolve(__dirname, '../resources/'),
+    source: path.resolve(__dirname, '../src/'),
 
     /* Path to built files directory */
     output: path.resolve(__dirname, '../dist/'),
