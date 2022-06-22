94889e1191f8a2e655db42bbe4fdf19787f4278e
youshaSilence <mksyousha2014@gmail.com>
2022-06-22 15:04:03 +0300

    Build for github pages

--- configuration/environment.js
+++ configuration/environment.js
@@ -6,7 +6,7 @@ module.exports = {
     source: path.resolve(__dirname, '../src/'),
 
     /* Path to built files directory */
-    output: path.resolve(__dirname, '../dist/'),
+    output: path.resolve(__dirname, '../docs/'),
   },
   server: {
     host: 'localhost',
